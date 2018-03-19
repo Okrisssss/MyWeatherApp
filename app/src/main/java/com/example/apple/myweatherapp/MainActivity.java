08848888a8a6f345f1b3d0693029ba9da8be6ddf
Petru Iachimov <p.iachimov199302@gmail.com>
2018-03-19 21:15:40 +0200

    Git init Googel MapActivity

--- app/src/main/java/com/example/apple/myweatherapp/MainActivity.java
+++ app/src/main/java/com/example/apple/myweatherapp/MainActivity.java
@@ -1,13 +1,195 @@
 package com.example.apple.myweatherapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.apple.weatherapplication.R;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+    private double latitude;
+    private double longitude;
+    private String address = "";
+    String currentDate = "";
+    Geocoder geocoder;
+    TextView addressTextView, dateTextView;
+    EditText cityEditText;
+    Location location;
+
+    static TextView nameTextView, temperatureTextView;
+
+/*    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        }
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addressTextView = (TextView) findViewById(R.id.addressTextView);
+        dateTextView = (TextView) findViewById(R.id.dateTextView);
+        cityEditText = (EditText) findViewById(R.id.cityEditText);
+        nameTextView = (TextView) findViewById(R.id.nameTextView);
+        temperatureTextView = (TextView) findViewById(R.id.temperatureTextView);
+
+        Calendar calendar = Calendar.getInstance();
+        currentDate = DateFormat.getDateInstance(DateFormat.LONG).format(calendar.getTime());
+        dateTextView.setText(currentDate);
+
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        String provider = locationManager.getBestProvider(new Criteria(), false);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+
+
+/*
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+            Criteria criteria = new Criteria();
+            String bestProvider = String.valueOf(locationManager.getBestProvider(criteria, true)).toString();
+
+//You can still do this if you like, you might get lucky:
+            Location location = locationManager.getLastKnownLocation(bestProvider);
+            if (location != null) {
+                Log.e("TAG", "GPS is on");
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+                Toast.makeText(MainActivity.this, "latitude:" + latitude + " longitude:" + longitude, Toast.LENGTH_SHORT).show();
+            }
+            else{
+//This is what you need:
+                locationManager.requestLocationUpdates(bestProvider, 1000, 0, locationListener);
+            }
+*/
+
+
+        DownloadTask task = new DownloadTask();
+        try {
+
+            task.execute("http://openweathermap.org/data/2.5/weather?lat=" + String.valueOf(latitude) + "&lon=" + String.valueOf(longitude) + "&appid=b6907d289e10d714a6e88b30761fae22").get();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+        geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        try {
+
+            List<Address> listAddresses = geocoder.getFromLocation(latitude, longitude, 1);
+
+            if (listAddresses != null && listAddresses.size() > 0) {
+
+                //Log.i("Place info", listAddresses.get(0).toString());
+
+                if (listAddresses.get(0).getLocality() != null) {
+
+                    address += listAddresses.get(0).getLocality() + ", ";
+
+                }
+
+                if (listAddresses.get(0).getCountryName() != null) {
+
+                    address += listAddresses.get(0).getCountryName();
+                    addressTextView.setText(address);
+                    address = "";
+
+                }
+
+            }
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+        locationListener = new LocationListener() {
+
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        //If device  is tunning SDK < 23
+
+/*        if (Build.VERSION.SDK_INT < 23) {
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        } else {
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // ask for permission
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+            } else {
+
+                //we have permission!
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }*/
     }
 }
+
+
