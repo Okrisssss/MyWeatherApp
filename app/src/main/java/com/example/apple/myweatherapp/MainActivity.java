c3f1b112e98e18fa8dd72726fc01db8a86d97533
Petru Iachimov <p.iachimov199302@gmail.com>
2018-03-24 10:31:18 +0200

    getting current name position

--- app/src/main/java/com/example/apple/myweatherapp/MainActivity.java
+++ app/src/main/java/com/example/apple/myweatherapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.apple.myweatherapp;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -20,6 +22,10 @@ import android.widget.TextView;
 import com.example.apple.weatherapplication.BuildConfig;
 import com.example.apple.weatherapplication.R;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
   private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -33,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
   private String mLongitudeLabel;
   private TextView mLatitudeText;
   private TextView mLongitudeText;
+  private TextView mAddressText;
   private LocationManager mLocationManager;
   private LocationListener mLocationListener;
 
@@ -51,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
       @Override
       public void onLocationChanged(final Location location) {
         if (location != null) {
+          updateLocationName(location);
           mLatitudeText.setText(mLatitudeLabel + " is " + String.valueOf(location.getLatitude()));
           mLongitudeText.setText(mLongitudeLabel + " is " + String.valueOf(location.getLongitude()));
         } else {
@@ -217,6 +225,44 @@ public class MainActivity extends AppCompatActivity {
                   }
                 });
       }
+    }
+  }
+  public void updateLocationName(Location location) {
+
+    Log.i("LocationInfo", location.toString());
+
+    Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+    try {
+
+      String address = "Could not find address";
+
+      List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+      if (listAddresses != null && listAddresses.size() > 0) {
+
+        Log.i("PlaceInfo", listAddresses.get(0).toString());
+
+        address = "Address: ";
+
+        if (listAddresses.get(0).getLocality() != null) {
+
+          address += listAddresses.get(0).getLocality() + ", ";
+
+        }
+
+        if (listAddresses.get(0).getCountryName() != null) {
+
+          address += listAddresses.get(0).getCountryName() ;
+
+        }
+
+      }
+      mAddressText = (TextView)findViewById(R.id.addres_text);
+      mAddressText.setText(address);
+
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 }
